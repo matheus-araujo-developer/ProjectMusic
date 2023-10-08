1639864d5ca2dc5fb052eaa7ebdcb22b65b42349
Matheus Araujo de Sousa <matheus2001sousa@hotmail.com>
2023-10-07 22:32:35 -0300

    adicionando classes

--- SistemaDeMusica/Musica.cs
+++ SistemaDeMusica/Musica.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SistemaDeMusica
+﻿class Musica
 {
-    internal class Musica
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}"; //Exemplo de uma Lambda
+
+    /*public void SetDisponivel(bool value)
     {
+        disponivel = value;
+    }
+    public bool GetDisponivel()
+    {
+        return disponivel;
+    }
+    */ //Esse trecho de código pode ser resumido do jeito que está na linha 6
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
     }
 }
--- SistemaDeMusica/Program.cs
+++ SistemaDeMusica/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Album album1 = new Album();
+album1.Nome = "Nirvana";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Come as you are";
+musica1.Duracao = 458;
+
+album1.AdicionarMusica(musica1);
+album1.ExibirMusicasDoAlbum();
